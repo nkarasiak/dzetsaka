9fbf779a07904214b202aad067e32197f4d73f21
Nicolas Karasiak <nicart@gmail.com>
2026-02-06 07:17:52 +0100

    chore: remove unused experimental processing algorithms

--- processing/closing_filter.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility shim for the migrated closing-filter algorithm."""
-
-from __future__ import annotations
-
-from dzetsaka.presentation.qgis.processing.algorithms.closing_filter import ClosingFilterAlgorithm
-
-__all__ = ["ClosingFilterAlgorithm"]
